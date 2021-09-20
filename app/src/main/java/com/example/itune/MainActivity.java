dcdff9281eed63a79255261168346e7102a5b167
MariannSF <mariann.freund@gmail.com>
2021-09-20 15:45:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/itune/MainActivity.java
@@ -0,0 +1,22 @@
+
+/*
+In Class 05
+MainActivity.java
+Mariann Szabo-Freund
+Sai Datta Bhaskararayuni
+ */
+
+package com.example.itune;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
